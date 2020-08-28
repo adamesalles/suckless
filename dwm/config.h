fd8891f621eedf14f3cead2dec9c14032e638ab0
adamesalles <eadamesalles@gmail.com>
2020-08-28 15:18:39 -0300

    Solving some shortcuts and rules in this change

--- dwm/config.h
+++ dwm/config.h
@@ -36,7 +36,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "termite", NULL };
-static const char *browsercmd[] = {"chromium" , NULL};
+static const char *browsercmd[] = {"firefox" , NULL};
 static const char *nmtuicmd[] = {"termite", "-e", "nmtui", NULL};
 static const char *pavucontrolcmd[] = {"pavucontrol", NULL};
 static const char *pcmanfmcmd[] = {"pcmanfm", NULL};
