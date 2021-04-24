a9a867d9dafbbdebab622ac12750655f65553525
adamesalles <eadamesalles@gmail.com>
2021-04-24 20:08:50 -0300

    Adicionando configurações para endeavour

--- dwm/config.h
+++ dwm/config.h
@@ -36,7 +36,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Google-chrome",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -50,7 +50,7 @@ static const Layout layouts[] = {
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "|M|",      centeredmaster },
-        { ">M>",      centeredfloatingmaster },
+    { ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "termite", NULL };
-static const char *browsercmd[] = {"firefox" , NULL};
+static const char *browsercmd[] = {"google-chrome-stable" , NULL};
 static const char *nmtuicmd[] = {"termite", "-e", "nmtui", NULL};
 static const char *pavucontrolcmd[] = {"pavucontrol", NULL};
 static const char *pcmanfmcmd[] = {"pcmanfm", NULL};
